eba1577bc234291a8dcd1e267deeee13b3c6c3dd
Jun Tsai <jcai@ganshane.com>
2014-10-05 10:39:54 +0800

    debug 14

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,16 @@ mkdir -p target
 cd target
 git clone git@bitbucket.org:jcai/nirvana.git
 cd nirvana
-mvn compile
+mkdir nirvana-c/build
+mkdir support/dll
+cd nirvana-c/build
+rm -rf *
+CC=/usr/bin/gcc44 cmake -DSTATIC_LINK=on -DCMAKE_BUILD_TYPE=Release ..
+#CC=gcc-4.1 CXX=g++-4.3 cmake  ..
+make
+cp src/*.so ../../support/dll/
+rm -rf ../../nirvana-jni/src/main/java/nirvana/jni/services/gen
+mkdir -p ../../nirvana-jni/src/main/java/nirvana/jni/services/gen
+cp src/javaapi/* ../../nirvana-jni/src/main/java/nirvana/jni/services/gen
+cd -
+mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=`date +%Y%m%d_%H%M%S` -DBUILD_NUMBER=$BUILD_NUMBER -P production clean package
