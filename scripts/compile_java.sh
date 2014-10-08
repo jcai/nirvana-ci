e935ce06db079d616259efc799d1d3302486e1ef
Jun Tsai <jcai@ganshane.com>
2014-10-08 14:15:34 +0800

    加大编译时候使用的内存空间

--- scripts/compile_java.sh
+++ scripts/compile_java.sh
@@ -1,2 +1,2 @@
 cd target/nirvana
-mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=`date +%Y%m%d_%H%M%S` -DBUILD_NUMBER=$BUILD_NUMBER -P production clean package
+MVN_OPTS="-Xms384M -Xmx512M -XX:MaxPermSize=256M" mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=`date +%Y%m%d_%H%M%S` -DBUILD_NUMBER=$BUILD_NUMBER -P production clean package
