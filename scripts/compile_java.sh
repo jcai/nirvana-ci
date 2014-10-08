f85aaf19fe8fdca2095a6fc04f7adb46ed256bcc
Jun Tsai <jcai@ganshane.com>
2014-10-08 15:15:51 +0800

    debug maven

--- scripts/compile_java.sh
+++ scripts/compile_java.sh
@@ -1,2 +1,2 @@
 cd target/nirvana
-MVN_OPTS="-Xmx1g" mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=`date +%Y%m%d_%H%M%S` -DBUILD_NUMBER=$BUILD_NUMBER -P production clean package
+MVN_OPTS="-Xmx1g" mvn -X -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=`date +%Y%m%d_%H%M%S` -DBUILD_NUMBER=$BUILD_NUMBER -P production clean package
