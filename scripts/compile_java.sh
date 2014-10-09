77473058d6f95e31ce2a65aa9d2c133d65e5a868
Jun Tsai <jcai@ganshane.com>
2014-10-09 12:59:41 +0800

    在非发布模式下仅仅编译，以提高集成化速度

--- scripts/compile_java.sh
+++ scripts/compile_java.sh
@@ -1,2 +1,6 @@
 cd target/nirvana
-MVN_OPTS="-Xmx1g" mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=`date +%Y%m%d_%H%M%S` -DBUILD_NUMBER=$BUILD_NUMBER -P production clean package
+if [ $TRAVIS_TAG ];then
+  MVN_OPTS="-Xmx1g" mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=`date +%Y%m%d_%H%M%S` -DBUILD_NUMBER=$BUILD_NUMBER -P production clean package
+else
+  MVN_OPTS="-Xmx1g" mvn -Dmaven.test.skip=true -DskipTests=true -Darguments="-DskipTests" -DBUILD_ID=`date +%Y%m%d_%H%M%S` -DBUILD_NUMBER=$BUILD_NUMBER -P production clean compile
+fi
