b6597c425d3d0ec9fdceba3c2921247f3fbc9396
Jun Tsai <jcai@ganshane.com>
2014-10-09 12:09:23 +0800

    加入部署的配置说明

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,6 +1,7 @@
+mkdir -p target
 if [ $TRAVIS_TAG ];then
   echo "checkout ${TRAVIS_TAG} ..."
-  git clone --branch=$TRAVIS_TAG $GIT_URL target/nirvana
+  git clone --branch=nirvana-project-$TRAVIS_TAG $GIT_URL target/nirvana
 else
   echo "checkout develop ..."
   git clone -b develop $GIT_URL target/nirvana
